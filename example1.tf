f497044198e6ce1c7be038f08be9a30445a5256b
Maxim <72321151+MaximSevko@users.noreply.github.com>
2022-06-07 18:46:05 +0300

    Update example1.tf

--- example1.tf
+++ example1.tf
@@ -151,7 +151,7 @@ resource "aws_instance" "AmazonLinux" {
     ami = "${lookup(var.AMI1, "AmazonLinux")}"
     instance_type = var.instance_type
     # VPC
-    subnet_id = aws_subnet.subnet_public[each.value]
+    subnet_id = aws_subnet.subnet_public.id
     # Security Group
     vpc_security_group_ids = ["${aws_security_group.webserver.id}"]
     # the Public SSH key
@@ -164,7 +164,7 @@ resource "aws_instance" "Ubuntu" {
     ami = "${lookup(var.AMI1, "Ubuntu")}"
     instance_type = var.instance_type
     # VPC
-    subnet_id = aws_subnet.subnet_public[each.value]
+    subnet_id = aws_subnet.subnet_public.id
     # Security Group
     vpc_security_group_ids = ["${aws_security_group.webserver.id}"]
     # the Public SSH key
