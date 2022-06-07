2ef605460567a54b5edeaa128a628194d2c96360
Maxim <72321151+MaximSevko@users.noreply.github.com>
2022-06-07 17:59:05 +0300

    Update example1.tf

--- example1.tf
+++ example1.tf
@@ -9,19 +9,27 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "subnet_public" {
-    for_each = var.availability_zones
+    //for_each = var.availability_zones
 
     vpc_id = "${aws_vpc.main.id}"
 
-    availability_zone = each.key
-    cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, each.value)
-    ipv6_cidr_block   = cidrsubnet(aws_vpc.main.ipv6_cidr_block, 8, each.value + 6)
+    availability_zone = eu-central-1a
 
+    //availability_zone = each.key
+    //cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, each.value)
+    //ipv6_cidr_block   = cidrsubnet(aws_vpc.main.ipv6_cidr_block, 8, each.value + 6)
+
+    cidr_block = "10.9.1.0/24"
+
+
+ 
     map_public_ip_on_launch = "true" //it makes this a public subnet
     assign_ipv6_address_on_creation = true
 
     tags = {
-        Name = "subnet-public-${each.value}"
+        //Name = "subnet-public-${each.value}"
+        Name = "subnet-public-1"
+
     }
 }
 
